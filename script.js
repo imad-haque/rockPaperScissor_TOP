37fae8f5a3e3bfdda6eeb275768d744bf211bfa9
Imad <imadhaque20@gmail.com>
2025-08-12 15:40:03 +0530

    optimized js code

--- script.js
+++ script.js
@@ -1,77 +1,64 @@
-    function getComputerChoice(){
-        let computerChoice = Math.floor(Math.random() * 100); 
+function getComputerChoice(){
+    let computerSelection = Math.floor(Math.random() * 100);
 
-        if (computerChoice <= 25){
-            computerChoice = "rock";
-            console.log("Computer Choice : Rock");
-        } else if(computerChoice <= 65){
-            computerChoice = "paper";
-            console.log("Computer Choice : Paper");
-        } else if (computerChoice <= 100){
-            computerChoice = "scissors";
-            console.log("Computer Choice: Scissors");
-        } 
-        return computerChoice;
-
+    if (computerSelection <= 25){
+        console.log("Computer's Chooses Rock!");
+        return "rock";
+    } else if(computerSelection <= 50){
+        console.log("Computer Chooses Paper!");
+        return "paper";
+    } else if(computerSelection <= 75){
+        console.log("Computer Chooses Scissors!");
+        return "scissors";
+    } 
+}
+function getHumanChoice(){
+    let humanSelection = prompt("Choose rock, paper or scissors!");
+    if(humanSelection === "rock"){
+        console.log("Human Chooses Rock!");
+    } else if (humanSelection === "paper"){
+        console.log("Human chooses paper!");
+    } else if(humanSelection === "scissors"){
+        console.log("Human Chooses Scissors!");
+    } else {
+        console.error("Invalid Choice Mate");
     }
+    return humanSelection.toLowerCase();
+}
 
-    function getHumanChoice(){
-        let humanChoice =  prompt("Choose your option: (ROCK, PAPER, SCISSORS)");
-        console.log(prompt);
-        if(humanChoice === "rock"){
-            console.log("Your Choice: Rock");
-        } else if (humanChoice === "paper"){  
-            console.log("Your Choice: Paper");
-        } else if (humanChoice === "scissors"){
-            console.log("Your Choice: Scissors");
-        } else {
-            console.log("Invalid Choice");
-        }
-        return humanChoice;
-    }
+let humanScore = 0;
+let ComputerScore = 0;
 
-    let humanScore = 0;
-    let computerScore = 0;
+function playRound(humanChoice, computerChoice){
+  if (humanChoice === computerChoice) {
+    console.log("Its a Tie!!");
+  } else if((humanChoice === "rock" && computerChoice === "scissors" || humanChoice === "paper" && computerChoice === "rock" || humanChoice === "scissors" && computerChoice === "paper")) {
+    console.log("Human Wins!");
+    humanScore++;
+  } else {
+    console.log("Computer Wins!");
+    ComputerScore++;
+  }
+}
 
-    function playRound (humanChoice, computerChoice){
-        if (humanChoice === "rock" && computerChoice === "scissors"){
-            console.log("Human Wins, Rock Beats Scissors L Computer!!!");
-            humanScore++;
-        }
-            else if(humanChoice === "scissors" && computerChoice === "paper"){
-                console.log("Human Wins, Scissors Beats Paper L Computer!!!");
-                humanScore++;
-            } else if (humanChoice === "paper" && computerChoice === "rock"){
-                console.log("Human Wins, Paper Beats Rock L Computer!!!");
-                humanScore++;
-            } else if (humanChoice === "rock" && computerChoice === "paper"){
-                console.log("Computer Wins, Paper Beats Rock L Human!!!");
-                computerScore++;
-            } else if(humanChoice === "paper" && computerChoice === "scissors"){
-                console.log("Computer Wins, scissors Beats paper L Human!!!");
-                computerScore++;
-            } else if(humanChoice === "scissors" && computerChoice === "rock"){
-                console.log("Computer Wins, Rock Beats Scissors L Human!!!");
-                computerScore++;
-            } else if(humanChoice === computerChoice){
-                console.log("Draw");
-            }
-        }
 
-    function playGame(){
-        for (let i = 0; i < 5; i++){
-            let humanChoice = getHumanChoice();
-            let computerChoice = getComputerChoice();
-            playRound(humanChoice, computerChoice);
-        }
-        if (humanScore > computerScore){
-            console.log("Human Wins. Human's Score is " + humanScore + "to Computer's " + computerScore);
-        } else if(humanScore < computerScore){
-            console.log("Computer Wins. Computer's score is " + computerScore + "to Human's " + humanScore );
-        } else if (humanScore === computerScore){
-            console.log("Nobody wins L Bozos.");
-        }
-    }
 
-    playGame();
+function playGame(){
+    for (let i = 0; i < 5; i++){
+       
+    const humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice();
+        playRound(humanSelection, computerSelection);
+}
+
+if (humanScore > ComputerScore){
+    console.log(`Human wins ${humanScore} : ${ComputerScore}`);
+} else if (humanScore < ComputerScore){
+    console.log(`Computer Wins ${ComputerScore} : ${humanScore}`);
+} else {
+    console.log("Nobody Wins");
+}
+}
+
+playGame();
 
